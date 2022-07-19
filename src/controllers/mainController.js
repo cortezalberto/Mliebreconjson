1abaedaf09aa4b582d10953c7fcddb6bc699f998
cortezalberto <cortezalberto@gmail.com>
2022-07-19 14:17:04 -0300

    Primer commit

---
+++ src/controllers/mainController.js
@@ -0,0 +1,17 @@
+
+const jsonDB = require('../model/jsonDatabase');
+const productModel = jsonDB('products')
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const mainController = {
+    index : (req,res)=>{
+		
+        const  visited = productModel.visited('visited')
+        const inSale = productModel.inSale('in-sale')
+        res.render('index', { visited, inSale,toThousand})
+    }
+}
+
+
+
+
+module.exports = mainController;
